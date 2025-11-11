b4aa17e38d5a60a455d1ca21502e5b68856a009a
NabillaChai <bilanisa23@gmail.com>
2025-11-11 08:28:11 +0700

    update cetak_pengaduan.php

--- admin/cetak_pengaduan.php
+++ admin/cetak_pengaduan.php
@@ -1,112 +1,175 @@
-<?php
-session_start();
-include '../koneksi.php';
-if(!isset ($_SESSION['level'])){
-	header ("location: index.php");
-	}else{
-?>
 <!DOCTYPE html>
-
+<html lang="id">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
-
-    <style>
-        @charset "utf-8";
-        /* CSS Document */
-        .tabel{
-            margin-left:50px;
-            }
-        .kotak{
-            padding: px 0px;
-            }
-        .titik{
-            margin-left:60%;
-            }
-
-
-    </style>
-    <link rel="icon" href="../img/lpg.png">
-</head>
-
-<body>
-    <style type="text/css">
-    .head {
-        width: 100%;
-        height: 100px;
-        border-bottom: 1px solid;
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cetak Laporan Pengaduan</title>
+  <link rel="icon" href="../img/logo-bandarlampung.png">
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: "Poppins", sans-serif;
     }
-    </style>
-    <div class="head">
-        <table>
-            <tr>
-                <td width="10%"><img src="../img/logo-bandarlampung.png" width="70px" height="80px" /></td>
-                <td align="center" width="90%">
-                    <font style="font-size:22px; font-family:Arial, Helvetica, sans-serif;">PEMERINTAH KABUPATEN
-                        LAMPUNG<br />Tanjung Karang Barat<br />sukajawa</font><br /><i>
-                        <font style="font-size:13px;">Gg. Balai desa, Sukajawa, Kec. TKB, Kota Bandar
-                            Lampung, Lampung 35141</font>
-                        <br>
-                        <br>
-                    </i>
-                </td>
-            </tr>
-        </table>
-    </div><br /><br /><br>
-    <div style="font-size:24px; text-align:center;">Laporan Pengaduan Masyarakat</div><br /><br />
-   
-    <table align="center" border="1" cellpadding="8" cellspacing="0">
-        <tr>
-            <th>No</th>
-            <th>Tgl Pengaduan</th>
-            <th>NIK</th>
-            <th>Isi Laporan</th>
-            <th>Foto</th>
-        </tr>
-        <?php
-$no = 1;
-$query = mysqli_query ($conn, "SELECT * FROM pengaduan WHERE tgl_pengaduan >= '" .$_POST['tgl1']. "' AND tgl_pengaduan <= '" .$_POST['tgl2']. "'");
-if (mysqli_num_rows ($query)){
-while ($data = mysqli_fetch_array ($query)) {
-?>
-        <tr align="left">
-        <tr>
-            <td><?php echo $no++ ?></td>
-            <td><?php echo $data['tgl_pengaduan'] ?></td>
-            <td><?php echo $data['nik'] ?></td>
-            <td><?php echo $data['isi_laporan'] ?></td>
-            <td><img src="<?= $data['foto']?>" width="100px"></td>
-        </tr>
-        <?php }}else{
-	echo '<tr><td colspan="6" align="center">TIDAK ADA DATA</td></tr>';
-}?>
-    </table>
+    body {
+      background: white;
+      color: black;
+      padding: 40px;
+    }
+    .kop-surat {
+      display: flex;
+      align-items: center;
+      border-bottom: 3px solid #000;
+      padding-bottom: 10px;
+      margin-bottom: 20px;
+    }
+    .kop-surat img {
+      width: 80px;
+      margin-right: 20px;
+    }
+    .kop-teks {
+      text-align: center;
+      flex: 1;
+    }
+    .kop-teks h2 {
+      font-size: 20px;
+      text-transform: uppercase;
+      font-weight: 700;
+    }
+    .kop-teks h3 {
+      font-size: 18px;
+      font-weight: 600;
+    }
+    .kop-teks p {
+      font-size: 13px;
+      margin-top: 5px;
+    }
+    .judul {
+      text-align: center;
+      font-weight: 600;
+      font-size: 18px;
+      margin-top: 30px;
+      margin-bottom: 20px;
+      text-transform: uppercase;
+    }
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-top: 10px;
+    }
+    th, td {
+      border: 1px solid #000;
+      padding: 8px 12px;
+      text-align: center;
+      font-size: 14px;
+    }
+    th {
+      background-color: #f2f2f2;
+      font-weight: 600;
+    }
+    td img {
+      width: 90px;
+      height: 70px;
+      object-fit: cover;
+      border-radius: 4px;
+    }
+    .ttd {
+      margin-top: 60px;
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+    }
+    .ttd .kolom {
+      text-align: center;
+      margin-right: 80px;
+    }
+    .ttd .kolom p {
+      margin-top: 5px;
+      font-size: 14px;
+    }
+    .ttd .nama {
+      margin-top: 60px;
+      font-weight: 600;
+      text-decoration: underline;
+    }
+    @media print {
+      body { padding: 0; }
+      .ttd { margin-top: 40px; }
+    }
+  </style>
+</head>
+<body>
 
-    <br />
+  <div class="kop-surat">
+    <img src="../img/logo-bandarlampung.png" alt="Logo Kota Bandar Lampung">
+    <div class="kop-teks">
+      <h2>PEMERINTAH KOTA BANDAR LAMPUNG</h2>
+      <h3>KECAMATAN TANJUNG KARANG BARAT</h3>
+      <p>Jl. Gg. Balai Desa, Sukajawa, Tanjung Karang Barat, Kota Bandar Lampung 35141</p>
+    </div>
+  </div>
 
-    <table class="titik">
-        <tr>
-            <td>&nbsp;&nbsp;&nbsp;&nbsp;BandarLampung,......................<script>
-                document.write(new Date().getFullYear());
-                </script>
-            </td>
-        </tr>
-        <tr>
-            <td>&nbsp;&nbsp;&nbsp;&nbsp;KEPALA KELURAHAN</td>
-        </tr>
-    </table>
+  <div class="judul">Laporan Pengaduan Masyarakat</div>
 
-    <br /><br /><br /><br />
-    <table class="titik">
-        <tr>
-            <td><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>. . . . . . . . . . . . . . . . . . .
-                        . . .</u></b></td>
-        </tr>
-    </table>
-    <script>
-    window.print()
-    </script>
+  <table id="tabel-laporan">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Tanggal Pengaduan</th>
+        <th>NIK</th>
+        <th>Isi Laporan</th>
+      </tr>
+    </thead>
+    <tbody></tbody>
+  </table>
+
+  <div class="ttd">
+    <div class="kolom">
+      <p id="tanggal-ttd"></p>
+      <p>KEPALA KELURAHAN</p>
+      <div class="nama">_________________________</div>
+    </div>
+  </div>
+
+  <script>
+    window.onload = function() {
+      const data = JSON.parse(localStorage.getItem("dataCetak") || "[]");
+      const tglAwal = localStorage.getItem("tglAwal");
+      const tglAkhir = localStorage.getItem("tglAkhir");
+
+      const tbody = document.querySelector("#tabel-laporan tbody");
+      tbody.innerHTML = "";
+
+      if (data.length === 0) {
+        tbody.innerHTML = `<tr><td colspan="4">Tidak ada data ditemukan.</td></tr>`;
+      } else {
+        data.forEach((item, i) => {
+          const row = `<tr>
+              <td>${i + 1}</td>
+              <td>${item.tanggal}</td>
+              <td>${item.nik}</td>
+              <td>${item.isi}</td>
+            </tr>`;
+          tbody.insertAdjacentHTML("beforeend", row);
+        });
+      }
+
+      // Tanggal tanda tangan otomatis
+      const ttd = document.getElementById("tanggal-ttd");
+      const tglCetak = tglAkhir
+        ? new Date(tglAkhir).toLocaleDateString("id-ID", {
+            day: "2-digit", month: "long", year: "numeric"
+          })
+        : new Date().toLocaleDateString("id-ID", {
+            day: "2-digit", month: "long", year: "numeric"
+          });
+      ttd.textContent = `Bandar Lampung, ${tglCetak}`;
+
+      // Cetak otomatis
+      window.print();
+    };
+  </script>
+
 </body>
-
 </html>
-<?php } ?>
